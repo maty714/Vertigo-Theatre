ef5f599a36530e56636c219ccfa2d2f4c5aa7bf9
Matt <87787075+maty714@users.noreply.github.com>
2022-04-09 13:23:24 -0400

    Update and rename TextHelper.cs to Text Helper/TextHelper.cs

---
+++ Text Helper/TextHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelper
+    {
+        public static string helper(string input, int num)
+        {
+
+            //converts a string to a character array
+            char[] ch = input.ToCharArray();
+
+
+            //loops through the array from the last element to the first, and replaces each element with a "."
+            for (int i = ch.Count(); i > num; i--)
+            {
+                    
+                //if (ch[i - 1] == ' ')
+                //{
+                //    continue;
+                //}
+                ch[i - 1] = '.';
+
+            }
+
+            //converts character array back to string to be returned by the method
+            string output = new string(ch);
+            return output;        
+         }
+    }
+}
